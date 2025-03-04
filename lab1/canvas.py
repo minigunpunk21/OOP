af831be993d22bcafb0593b3fe3b36907d1cfaaf
minigunpunk21 <error.danilov5@mail.ru>
2025-03-04 18:21:07 +0000

    vrode norm canvas no ya hz

---
+++ lab1/canvas.py
@@ -0,0 +1,18 @@
+class Canvas:
+    def init(self):
+        self.figures=[]
+
+    def ass_figure(self,figure):
+        self.figures.append(figure)
+    
+    def remove_figure(self, index):
+        if 0<=index < len(self.figures):
+            del self.figures[index]
+    
+    def move_figure(self,index,new_x,new_y):
+        if 0<= index < len(self.figures):
+            self.figures[index].move(new_x,new_y)
+
+    def draw_canvas(self):
+        for figure in self.figures:
+            figure.draw()
