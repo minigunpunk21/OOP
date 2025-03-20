4d46b8b6b47121d670b1bcfc3f210eba336269e9
minigunpunk21 <error.danilov5@mail.ru>
2025-03-20 09:58:00 +0000

    velike nedarabotki

--- lab1/storage.py
+++ lab1/storage.py
@@ -6,8 +6,34 @@ class Storage:
     def save(canvas, filename: str) -> None:
         data = []
         for figure in canvas._figures:
-            d = figure.__dict__.copy()
-            d['_type'] = figure.__class__.__name__
+            # Use a dictionary to clearly define what to save
+            if isinstance(figure, Rectangle):
+                d = {
+                    '_type': 'Rectangle',
+                    'x': figure._x,
+                    'y': figure._y,
+                    'width': figure._width,
+                    'height': figure._height,
+                    'filled': figure.filled
+                }
+            elif isinstance(figure, Circle):
+                d = {
+                    '_type': 'Circle',
+                    'x': figure._x,
+                    'y': figure._y,
+                    'radius': figure._radius,
+                    'filled': figure.filled
+                }
+            elif isinstance(figure, Triangle):
+                d = {
+                    '_type': 'Triangle',
+                    'x': figure._x,
+                    'y': figure._y,
+                    'a': figure._a,
+                    'b': figure._b,
+                    'c': figure._c,
+                    'filled': figure.filled
+                }
             data.append(d)
         with open(filename, 'w') as f:
             json.dump(data, f)
@@ -21,11 +47,11 @@ class Storage:
                 for item in data:
                     ftype = item.pop('_type', None)
                     if ftype == 'Rectangle':
-                        figures.append(Rectangle(**item))
+                        figures.append(Rectangle(item['x'], item['y'], item['width'], item['height']))
                     elif ftype == 'Circle':
-                        figures.append(Circle(**item))
+                        figures.append(Circle(item['x'], item['y'], item['radius']))
                     elif ftype == 'Triangle':
-                        figures.append(Triangle(**item))
+                        figures.append(Triangle(item['x'], item['y'], item['a'], item['b'], item['c']))
             return figures
         except FileNotFoundError:
             print("Файл не найден.")
